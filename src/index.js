ff51fffa12656ac74627c2bb0ee36999a28e26bf
charlie <charliegogh@163.com>
2023-02-28 08:40:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./layout";
 import "./styles/index.less";
-// test 11 11
+// test
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(<App />);
