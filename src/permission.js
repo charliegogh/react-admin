6032de0fea51068aa6d97ff328754950d27f5856
charlie <charliegogh@163.com>
2023-03-02 17:07:52 +0800

    ~~~~

--- src/permission.js
+++ src/permission.js
@@ -1,26 +1,25 @@
-import { useLocation, Navigate } from "react-router-dom";
-import { getToken } from "./utils/auth";
+import { useLocation, Navigate } from 'react-router-dom'
+import { getToken } from './utils/auth'
 /*
   高阶组件HOC
     本质上是一个函数，接受组件作为参数，返回一个新组件
 */
 function permission(WrappedComponent) {
-  WrappedComponent.displayName = "permission";
+  WrappedComponent.displayName = 'permission'
   return () => {
-    console.log("~~~~");
-    const { pathname } = useLocation();
-    const token = getToken();
+    const { pathname } = useLocation()
+    const token = getToken()
     if (token) {
-      if (["/login", "/"].includes(pathname)) {
-        return <Navigate to="/home" />;
+      if (['/login', '/'].includes(pathname)) {
+        return <Navigate to='/home' />
       }
-      return <WrappedComponent />;
+      return <WrappedComponent />
     } else {
-      if (pathname === "/login") {
-        return <WrappedComponent />;
+      if (pathname === '/login') {
+        return <WrappedComponent />
       }
-      return <Navigate to="/login" />;
+      return <Navigate to='/login' />
     }
-  };
+  }
 }
-export default permission;
+export default permission
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react'
 import { getAction } from '../axios'
 export default function App() {
   useEffect(() => {
-    const getData = async() => {
-      const rs = await getAction('http://47.93.4.29:81/test/')
-      console.log(rs)
-    }
-    getData()
+    // const getData = async() => {
+    //   const rs = await getAction('http://47.93.4.29:81/test/')
+    // }
+    // getData()
+    console.log(111111)
   })
-  return <div className='App'>home1111111111</div>
+  return <div className='App'>111</div>
 }
